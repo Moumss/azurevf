b1445a21652940a8f32a3733e8f9394a85f217e0
SALAMI Abdelmoughit <XC6071@engie.com>
2021-02-19 12:57:28 +0100

    staticconfigurl

--- carbonlight/urls.py
+++ carbonlight/urls.py
@@ -5,3 +5,8 @@ urlpatterns = [
     path('data/', include('data.urls')),
     path('admin/', admin.site.urls),
 ]
+
+# for serving static files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# for serving media files (files that were uploaded through your project application)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
