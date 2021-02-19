55a4fbe0d00044a9b41a34ea337fe905514b7e80
SALAMI Abdelmoughit <XC6071@engie.com>
2021-02-19 15:35:35 +0100

    back to normal

--- carbonlight/urls.py
+++ carbonlight/urls.py
@@ -6,7 +6,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-# for serving static files
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# for serving media files (files that were uploaded through your project application)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
