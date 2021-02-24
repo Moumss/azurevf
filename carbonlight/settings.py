6bdfda72aa24cbe1e807045b216020627312cb4b
SALAMI Abdelmoughit <XC6071@engie.com>
2021-02-24 16:17:15 +0100

    back to normal

--- carbonlight/settings.py
+++ carbonlight/settings.py
@@ -32,7 +32,6 @@ INSTALLED_APPS = [
     'django.contrib.postgres',
     'django.contrib.staticfiles',
     
-    'corsheaders',
     'storages',
 
     'rest_framework',
@@ -46,7 +45,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
